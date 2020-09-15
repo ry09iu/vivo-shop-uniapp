21ece6cc2dd23487df451c854742a6e4c5d2b749
Ry09iu <sxpatch@sina.cn>
2020-09-15 11:32:30 +0800

    添加首页 banner 底部服务类

---
+++ pages/index/components/footer.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="footer-item">
+		<image class="icon" :src="img"></image>
+		<text class="title">{{title}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'BannerFooter',
+		props: {
+			title: {
+				type: String,
+				default: ''
+			},
+			img: {
+				type: String,
+				default: ''
+			}
+		},
+		data() {
+			return {}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.footer-item {
+		display: flex;
+		align-items: center;
+		height: 68rpx;
+
+		.icon {
+			width: 30rpx;
+			height: 30rpx;
+		}
+
+		.title {
+			margin-left: 12rpx;
+			color: $uni-text-color-assist;
+			font-size: 20rpx;
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,16 +2,40 @@
 	<view class="home">
 		<view class="banner">
 			<u-swiper :list="banners" :border-radius="0" height="500" name="imageUrl" indicator-pos="bottomRight"></u-swiper>
+
+			<view class="footer">
+				<view v-for="(item,index) in footers" :key="index">
+					<BannerFooter :title="item.title" :img="item.img" />
+				</view>
+			</view>
 		</view>
+
 	</view>
 </template>
 
 <script>
+	import BannerFooter from './components/footer.vue'
 	export default {
+		components: {
+			BannerFooter
+		},
 		data() {
 			return {
 				title: 'Hello',
 				banners: [],
+				footers: [{
+					img: '/static/images/home/banner/official.png',
+					title: '官方正品',
+				}, {
+					img: '/static/images/home/banner/sf.png',
+					title: '顺丰极速达',
+				}, {
+					img: '/static/images/home/banner/love.png',
+					title: '全国联保',
+				}, {
+					img: '/static/images/home/banner/flash.png',
+					title: '镭雕定制',
+				}]
 			}
 		},
 		async onLoad() {
@@ -24,7 +48,10 @@
 </script>
 
 <style lang="scss" scoped>
-	.u-swiper-indicator {
-		bottom: 52rpx !important;
+	.footer {
+		display: flex;
+		justify-content: space-around;
+		padding: 0 48rpx;
+		height: 68rpx;
 	}
 </style>
